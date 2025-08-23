4503344576508275f15c8533e8a4c8e43aedf0a6
Lumi <lumi.f3m@gmail.com>
2025-08-23 11:58:54 -0400

    pink shulker + more illegals!

--- illegalBlocks.js
+++ illegalBlocks.js
@@ -1,33 +1,13 @@
 player.onChat(".illegals_kit_2", function () {
-    player.say("Giving you the advanced illegals kit!");
+    player.say("Giving you The Anarchist's Toolbox!");
 
-    // Unobtainable blocks
-    player.execute("/give @p bedrock 64");
-    player.execute("/give @p end_portal_frame 64");
-    player.execute("/give @p barrier 64");
-    player.execute("/give @p water 64");
-    player.execute("/give @p fire 64");
+    // Gives a fully-stocked shulker box with a custom name
+    player.execute("/give @p pink_shulker_box{display:{Name:'{\"text\":\"Wowzerz\",\"color\":\"gold\"}'},BlockEntityTag:{Items:[{Slot:0,id:\"minecraft:bedrock\",Count:64},{Slot:1,id:\"minecraft:end_portal_frame\",Count:64},{Slot:2,id:\"minecraft:barrier\",Count:64},{Slot:3,id:\"minecraft:fire\",Count:64},{Slot:4,id:\"minecraft:command_block\",Count:64},{Slot:5,id:\"minecraft:repeating_command_block\",Count:64},{Slot:6,id:\"minecraft:chain_command_block\",Count:64},{Slot:7,id:\"minecraft:mob_spawner\",Count:1,tag:{BlockEntityTag:{SpawnData:{id:\"minecraft:pig\"}}}},{Slot:8,id:\"minecraft:player_head\",Count:1},{Slot:9,id:\"minecraft:dead_bush\",Count:1,tag:{display:{Name:'{\"text\":\"Shrubs\"}'}}},{Slot:10,id:\"minecraft:pig_spawn_egg\",Count:64},{Slot:11,id:\"minecraft:enchanted_book\",Count:1,tag:{Enchantments:[{id:\"sharpness\",lvl:32767}]}},{Slot:12,id:\"minecraft:glass\",Count:1,tag:{Enchantments:[{}]}},{Slot:13,id:\"minecraft:command_block_minecart\",Count:64},{Slot:14,id:\"minecraft:dragon_egg\",Count:1},{Slot:15,id:\"minecraft:structure_block\",Count:64},{Slot:16,id:\"minecraft:jigsaw\",Count:64},{Slot:17,id:\"minecraft:light\",Count:64},{Slot:18,id:\"minecraft:knowledge_book\",Count:1},{Slot:19,id:\"minecraft:nether_star\",Count:64},{Slot:20,id:\"minecraft:end_crystal\",Count:64},{Slot:21,id:\"minecraft:spawner\",Count:1,tag:{BlockEntityTag:{SpawnData:{id:\"minecraft:ender_dragon\"}}}},{Slot:22,id:\"minecraft:netherite_block\",Count:64},{Slot:23,id:\"minecraft:beacon\",Count:64},{Slot:24,id:\"minecraft:ender_eye\",Count:64},{Slot:25,id:\"minecraft:enchanted_golden_apple\",Count:64},{Slot:26,id:\"minecraft:potion\",Count:1,tag:{Potion:\"minecraft:strong_healing\"}}]}}");
 
-    // All three types of Command Blocks
-    player.execute("/give @p command_block 64");
-    player.execute("/give @p repeating_command_block 64");
-    player.execute("/give @p chain_command_block 64");
-
-    // A mob spawner that spawns pigs
-    player.execute("/give @p mob_spawner{BlockEntityTag:{SpawnData:{id:\"minecraft:pig\"}}}");
-
-    // Custom items and blocks
-    player.execute("/give @p player_head 1");
-    player.execute("/give @p dead_bush{display:{Name:'{\"text\":\"Shrubs\"}'}}");
-    player.execute("/give @p pig_spawn_egg 64");
-
-    // Illegal Enchanted Book and Enchanted Glass
-    player.execute("/give @p enchanted_book{Enchantments:[{id:\"sharpness\",lvl:32767}]}");
-    player.execute("/give @p glass{Enchantments:[{}]}");
-
-    // Command Block Minecart and a renamed shulker box
-    player.execute("/give @p command_block_minecart 64");
-    player.execute("/give @p dark_blue_shulker_box{display:{Name:'{\"text\":\"blocks\"}'}}");
+    // These items can't go in the shulker, so they are given separately
+    mobs.give(mobs.target(LOCAL_PLAYER), WATER, 64);
+    mobs.give(mobs.target(LOCAL_PLAYER), LAVA, 64);
+    mobs.give(mobs.target(LOCAL_PLAYER), FIRE, 64);
     
-    player.say("Your illegals blocks kit is ready!");
+    player.say("Your illegal blocks kit is ready.");
 });
