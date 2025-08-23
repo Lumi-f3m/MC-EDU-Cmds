a22ad1deff8a71837f74f2e7bd5108f2d9024259
Lumi <lumi.f3m@gmail.com>
2025-08-23 12:20:00 -0400

    Update opkit.js

--- opkit.js
+++ opkit.js
@@ -1,19 +1,20 @@
 player.onChat(".kit", function () {
-    player.say("Giving you a full kit of paw's op kit loot!");
+    player.say("Equipping paw's op kit loot!");
 
-    // Netherite Armor with Protection IV, Unbreaking III, and Mending I
-    player.execute("/give @p netherite_helmet{Enchantments:[{id:\"protection\",lvl:4},{id:\"unbreaking\",lvl:3},{id:\"mending\",lvl:1},{id:\"aqua_affinity\",lvl:1},{id:\"respiration\",lvl:3}],display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
-    player.execute("/give @p netherite_chestplate{Enchantments:[{id:\"protection\",lvl:4},{id:\"unbreaking\",lvl:3},{id:\"mending\",lvl:1}],display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
-    player.execute("/give @p netherite_leggings{Enchantments:[{id:\"protection\",lvl:4},{id:\"unbreaking\",lvl:3},{id:\"mending\",lvl:1}],display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
-    player.execute("/give @p netherite_boots{Enchantments:[{id:\"protection\",lvl:4},{id:\"unbreaking\",lvl:3},{id:\"mending\",lvl:1},{id:\"feather_falling\",lvl:4}],display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
+    // Armor with Protection IV, Unbreaking III, and Mending I
+    player.execute("/replaceitem entity @p slot.armor.head netherite_helmet 1 {Enchantments:[{id:\"protection\",lvl:4},{id:\"unbreaking\",lvl:3},{id:\"mending\",lvl:1},{id:\"aqua_affinity\",lvl:1},{id:\"respiration\",lvl:3}],display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
+    player.execute("/replaceitem entity @p slot.armor.chest netherite_chestplate 1 {Enchantments:[{id:\"protection\",lvl:4},{id:\"unbreaking\",lvl:3},{id:\"mending\",lvl:1}],display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
+    player.execute("/replaceitem entity @p slot.armor.legs netherite_leggings 1 {Enchantments:[{id:\"protection\",lvl:4},{id:\"unbreaking\",lvl:3},{id:\"mending\",lvl:1}],display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
+    player.execute("/replaceitem entity @p slot.armor.feet netherite_boots 1 {Enchantments:[{id:\"protection\",lvl:4},{id:\"unbreaking\",lvl:3},{id:\"mending\",lvl:1},{id:\"feather_falling\",lvl:4}],display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
 
-    // Max Enchanted Tools and a Trident
-    player.execute("/give @p netherite_sword{Enchantments:[{id:\"sharpness\",lvl:5},{id:\"unbreaking\",lvl:3},{id:\"mending\",lvl:1}],display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
-    player.execute("/give @p netherite_pickaxe{Enchantments:[{id:\"efficiency\",lvl:5},{id:\"unbreaking\",lvl:3},{id:\"mending\",lvl:1},{id:\"fortune\",lvl:3}],display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
+    // Max Enchanted Tools in hotbar
+    player.execute("/replaceitem entity @p slot.hotbar.0 netherite_sword 1 {Enchantments:[{id:\"sharpness\",lvl:5},{id:\"unbreaking\",lvl:3},{id:\"mending\",lvl:1}],display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
+    player.execute("/replaceitem entity @p slot.hotbar.1 netherite_pickaxe 1 {Enchantments:[{id:\"efficiency\",lvl:5},{id:\"unbreaking\",lvl:3},{id:\"mending\",lvl:1},{id:\"fortune\",lvl:3}],display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
+    player.execute("/replaceitem entity @p slot.hotbar.2 netherite_axe 1 {Enchantments:[{id:\"efficiency\",lvl:5},{id:\"unbreaking\",lvl:3},{id:\"mending\",lvl:1},{id:\"silk_touch\",lvl:1}],display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
+    player.execute("/replaceitem entity @p slot.weapon.offhand totem_of_undying 1");
+
+    // Other kit items that are given to inventory
     player.execute("/give @p trident{Enchantments:[{id:\"riptide\",lvl:3},{id:\"unbreaking\",lvl:3},{id:\"mending\",lvl:1}],display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
-
-    // Potion, Totem, and End-related items
-    player.execute("/give @p totem_of_undying 64 {display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
     player.execute("/give @p ender_pearl 64 {display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
     player.execute("/give @p end_crystal 64 {display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
     player.execute("/give @p enchanted_golden_apple 64 {display:{Name:'{\"text\":\"paw\\'s op kit loot\"}'}}");
